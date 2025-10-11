49d1ae145723cdc27db1fd4d2020f757a7c9a8b8
clariceleite <clariceleiterangel1@gmail.com>
2025-10-11 20:02:36 -0300

    Update AnaliseDeVendas.py

--- projeto-analise-dados/AnaliseDeVendas.py
+++ projeto-analise-dados/AnaliseDeVendas.py
@@ -8,49 +8,56 @@ dataframe = pd.read_csv("customer_journey.csv")
 print(dataframe.head())
 print("\n--- Tipos de Dados ---")
 print(dataframe.info()) 
-# %%
+#%%
+# Convertendo a coluna 'Timestamp' para o tipo datetime
 dataframe['Timestamp'] = pd.to_datetime(dataframe['Timestamp'])
 print("\n--- Contagem de Valores Ausentes ---")
 print(dataframe.isnull().sum())
-# %%
+#%%
 # Porcentagem de sessões que resultaram em compra por país.
-conversao_por_pais = dataframe.groupby('Country').agg(
-    TotalSessoes=('SessionID', 'count'),
-    TotalCompras=('Purchased', 'sum')
+conversao_pais = dataframe.groupby('Country').agg(
+    total_sessoes=('SessionID', 'nunique'),
+    total_compras=('Purchased', 'sum')
 )
-# %%
+
+#%%
 # Cálculo da Taxa de Conversão
-conversao_por_pais['TaxaConversao'] = (
-    conversao_por_pais['TotalCompras'] / conversao_por_pais['TotalSessoes']
-) * 100
-# %%
+conversao_pais['taxa_conversao'] = (conversao_pais['total_compras'] / conversao_pais['total_sessoes']) * 100
 # Classificação e Visualização
-conversao_por_pais = conversao_por_pais.sort_values(by='TaxaConversao', ascending=False)
+conversao_pais = conversao_pais.sort_values(by='taxa_conversao', ascending=False)
 print("\n--- Taxa de Conversão por País ---")
-print(conversao_por_pais)
+print(conversao_pais)
+
 #%%
 # Queremos saber onde os usuários passam mais tempo por tipo de dispositivo
 engajamento_dispositivo = dataframe.groupby('DeviceType')['TimeOnPage_seconds'].mean().sort_values(ascending=False)
 print("\n--- Tempo Médio na Página por Dispositivo ---")
 print(engajamento_dispositivo)
+
 # %%
 # Taxa de Conversão por País (Barra)
-plt.figure(figsize=(10, 6))
-conversao_por_pais['TaxaConversao'].plot(kind='bar', color= sns.color_palette("viridis", len(conversao_por_pais)))
+x_paises = conversao_pais.index
+y_taxas = conversao_pais['taxa_conversao']
+cores = sns.color_palette("viridis", len(conversao_pais))
+plt.figure(figsize=(9, 5))
+plt.bar(x_paises, y_taxas, color=cores)
 plt.title('Taxa de Conversão (%) por País')
 plt.ylabel('Taxa de Conversão (%)')
 plt.xlabel('País')
-plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
+
 # %%
 # Engajamento por Dispositivo (Barra)
+x_dispositivos = engajamento_dispositivo.index
+y_tempo = engajamento_dispositivo.values
+cores = ['lightcoral', 'lightskyblue', 'lightgreen']
 plt.figure(figsize=(8, 5))
-engajamento_dispositivo.plot(kind='bar', color=['lightcoral', 'lightskyblue', 'lightgreen'])
+plt.bar(x_dispositivos, y_tempo, color=cores)
+
 plt.title('Tempo Médio de Engajamento por Dispositivo')
 plt.ylabel('Tempo em Segundos (Média)')
 plt.xlabel('Tipo de Dispositivo')
-plt.xticks(rotation=0)
 plt.tight_layout()
 plt.show()
 # %%
